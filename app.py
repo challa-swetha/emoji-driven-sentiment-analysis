7e728c7eecaaccf493b5d22010d781c193be9881
challa_swetha <swethareddychalla09@gmail.com>
2025-05-31 00:00:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def index():
 
     return render_template("index.html", result=result)
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
